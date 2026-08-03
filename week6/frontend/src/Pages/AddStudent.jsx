a6e9241f09cbdd885cc3c873e5151f6e5e5491bd
Rajib Shrestha <166205605+Rajibstha01@users.noreply.github.com>
2026-08-03 09:12:49 +0545

    Delete week6 directory

--- week6/frontend/src/Pages/AddStudent.jsx
+++
@@ -1,6 +0,0 @@
-// src/pages/AddStudent.jsx
-import Form from "../components/Form/Form";
-function AddStudent() {
-return <Form />;
-}
-export default AddStudent;
--- week6/week5-SG2/middlewares/errorHandler.js
+++
@@ -1,9 +0,0 @@
-export const notFoundHanlder=(req,res)=>{
-    res.status(404).json({error:"route not found"})
-}
-
-
-export const errorHandler = (err, req, res, next) => {
-console.error(err.stack);
-res.status(500).json({ error: "Internal server error" });
-};
